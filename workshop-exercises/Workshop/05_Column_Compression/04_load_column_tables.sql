409730294e71a0816712c2712d9377828439e68e
mpresser-pivotal <mpresser@pivotal.io>
2018-07-10 12:55:10 -0400

    Update 04_load_column_tables.sql

--- workshop-exercises/Workshop/05_Column_Compression/04_load_column_tables.sql
+++ workshop-exercises/Workshop/05_Column_Compression/04_load_column_tables.sql
@@ -1,10 +1,10 @@
-insert into otp_c select * from otp_r;
+insert into faa.otp_c select * from faa.otp_r;
 analyze faa.otp_c;
 
-insert into otp_cc5 select * from otp_r;
+insert into faa.otp_cc5 select * from faa.otp_r;
 analyze faa.otp_cc5;
 
-insert into otp_cclz select * from otp_r;
+insert into faa.otp_cclz select * from faa.otp_r;
 analyze faa.otp_cclz;
 
 
