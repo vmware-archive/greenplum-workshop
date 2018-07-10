0f0641a95b4d9b05e4aad38f2b43b0506b1170a9
mpresser-pivotal <mpresser@pivotal.io>
2018-07-10 12:57:02 -0400

    Update 03_insert_data.sql

--- workshop-exercises/Workshop/09_External_Web_Tables/03_insert_data.sql
+++ workshop-exercises/Workshop/09_External_Web_Tables/03_insert_data.sql
@@ -1,3 +1,3 @@
-insert into wwearthquakes_lastwk select * from ext_wwearthquakes_lastwk;
-select count(*) total_records from wwearthquakes_lastwk;
-select * from wwearthquakes_lastwk order by 1 desc limit 100;
+insert into public.wwearthquakes_lastwk select * from public.ext_wwearthquakes_lastwk;
+select count(*) total_records from public.wwearthquakes_lastwk;
+select * from public.wwearthquakes_lastwk order by 1 desc limit 100;
