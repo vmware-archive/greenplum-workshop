ffe30f165e15859696c0d4d549e2e74ff220ab14
mpresser-pivotal <mpresser@pivotal.io>
2018-07-10 12:53:37 -0400

    Update 02_load_week_partitioned_table.sql

--- workshop-exercises/Workshop/04_Partitioning/02_load_week_partitioned_table.sql
+++ workshop-exercises/Workshop/04_Partitioning/02_load_week_partitioned_table.sql
@@ -1,8 +1,8 @@
 set search_path to faa;
 
-insert into otp_rpw select * from otp_r;
-analyze rootpartition otp_rpw;
+insert into faa.otp_rpw select * from faa.otp_r;
+analyze rootpartition faa.otp_rpw;
 
-select count(*) from otp_r;
-select count(*) from otp_rpw;
-select count(*) from otp_rpw_1_prt_out_of_range ;
+select count(*) from faa.otp_r;
+select count(*) from faa.otp_rpw;
+select count(*) from faa.otp_rpw_1_prt_out_of_range ;
